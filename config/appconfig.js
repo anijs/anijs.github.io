75711b3bb5a7c16cd32b378ffa71676e200b4fda
Dariel Noel <darielnoel@gmail.com>
2014-11-18 00:14:50 -0500

    Animation Names

--- config/appconfig.js
+++ config/appconfig.js
@@ -104,14 +104,14 @@ APP_CONFIG = {
     { do:'pulse animated', to:'$children .content-wrapper' },
     { do:'hinge animated', to:'$find body | .fa' },
     { do:'bounceIn animated', to:'$ancestors target | .container-box' },
-    { do:'flip animated', to:'$closest target' },
+    { do:'flip animated', to:'$closest target, after: $removeAnim' },
     { do:'wobble animated', to:'.container-box' },
-    { do:'rollOut animated', to:'.demo1' },
+    { do:'rollOut animated', to:'.demo1, after: $removeAnim' },
     { do:'hinge animated', to:'h3' },
     { do:'rotateIn animated', to:'$parent target' },
     { do:'$toggleClass invisible', to:'.demo-toggle' },
     { do:'$toggleClass pink-lgh-bg', to:'.demo1' },
     { do:'$toggleClass gray-bg', to:'$ancestors target' },
-    { do:'$removeClass gray-bg', to:'$ancestors target' },
+    { do:'$removeClass gray-bg', to:'$ancestors target' }
   ]
 };
